b167eec2bab4b168f991bca75b2ca5343d074277
Quan Nguyen <quannguyen@Quans-MacAir.attlocal.net>
2024-02-14 12:58:59 -0600

    Added error files

---
+++ public/error404/error404.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
